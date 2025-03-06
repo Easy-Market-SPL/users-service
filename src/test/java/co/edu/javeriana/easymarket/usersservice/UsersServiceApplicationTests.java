9086c518235b350e6cf00effa8dc17c529bf478f
Esteban Salazar <salazaraesteban@javeriana.edu.co>
2025-03-06 16:30:12 -0500

    Initial commit

---
+++ src/test/java/co/edu/javeriana/easymarket/usersservice/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.easymarket.usersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
